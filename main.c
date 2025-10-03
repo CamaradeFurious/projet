b308e02f2d79772eafef868ed7ef439b7b2e2541
CamaradeFurious <123994043+CamaradeFurious@users.noreply.github.com>
2025-10-03 13:58:17 +0000

    réctangle

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    // Dimensions du cercle
-    int largeur = 100;
+    // Dimensions du réctangle
     int hauteur = 100;
+    int largeur = 150;
 
     // Création du fichier HTML
-    FILE *f = fopen("cercle.html", "w");
+    FILE *f = fopen("réctangle.html", "w");
     if (f == NULL) {
         perror("Erreur lors de la création du fichier");
         return 1;
@@ -14,13 +14,13 @@ int main() {
 
     // Écriture du contenu HTML avec le SVG
     fprintf(f, "<!DOCTYPE html>\n");
-    fprintf(f, "<html lang=\"fr\">\n<head>\n<meta charset=\"UTF-8\">\n<title>cercle Bleue SVG</title>\n</head>\n<body>\n");
-    fprintf(f, "<svg width=\"%d\" height=\"%d\" xmlns=\"http://www.w3.org/2000/svg\">\n", largeur, hauteur);
-    fprintf(f, "  <ellipse cx=\"%d\" cy=\"%d\" rx=\"%d\" ry=\"%d\" fill=\"blue\" />\n", largeur/2, hauteur/2, largeur/2, hauteur/2);
+    fprintf(f, "<html lang=\"fr\">\n<head>\n<meta charset=\"UTF-8\">\n<title>Réctangle Bleu SVG</title>\n</head>\n<body>\n");
+    fprintf(f, "<svg width=\"%d\" height=\"%d\" xmlns=\"http://www.w3.org/2000/svg\">\n", hauteur, largeur);
+    fprintf(f, "  <rect x=\"0\" y=\"0\" width=\"%d\" height=\"%d\" fill=\"blue\" />\n", hauteur, largeur);
     fprintf(f, "</svg>\n");
     fprintf(f, "</body>\n</html>\n");
 
     fclose(f);
-    printf("Le fichier cercle.html a été généré.\n");
+    printf("Le fichier réctangle.html a été généré.\n");
     return 0;
 }
